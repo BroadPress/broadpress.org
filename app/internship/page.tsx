9e30e11d989bb8a74684b6d598854443c04d9751
PRACAS <pracas@Pracass-Mac-mini-3.local>
2026-03-15 09:30:24 +0545

    Internship form page added

---
+++ app/internship/page.tsx
@@ -0,0 +1,13 @@
+export default function TestimonialsPage() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-blue-100 to-blue-300 flex justify-center items-center">
+      <iframe
+        src="https://airtable.com/embed/appcRLi3R3qIFyPIU/pagv1BvN6pd0HZWwl/form"
+        frameBorder="0"
+        className="w-full h-full"
+        style={{ background: "transparent" }}
+        title="Embedded Airtable Form"
+      ></iframe>
+    </div>
+  )
+}
